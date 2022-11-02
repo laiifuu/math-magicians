d2af54a594cb53a041f5a76064c0dd44b4c414fe
natanimabesha <natanimabesha@www.zhewutech.com>
2022-11-02 16:59:32 +0300

    test pass for counting links

--- src/components/__test__/Header.test.js
+++ src/components/__test__/Header.test.js
@@ -1,10 +1,22 @@
 import Header from '../Header'
 import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe("Tests Header Component", () => {
     it('should render Header component', () => {
         const tree = renderer.create(<BrowserRouter ><Header /></BrowserRouter>).toJSON();
         expect(tree).toMatchSnapshot();
+    });
+
+    it(' adds the active class to the clicked link', () => {
+        render(
+            <BrowserRouter>
+                <Header />
+            </BrowserRouter>,
+        );
+        const list = screen.getAllByRole('link');
+        expect(list.length).toEqual(3);
     });
 })
