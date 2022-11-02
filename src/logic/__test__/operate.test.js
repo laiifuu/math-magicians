0e791c5ae4669af942011a4c0feab9abecb7e402
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-02 14:48:53 +0100

    Fix linter errors

--- src/logic/__test__/operate.test.js
+++ src/logic/__test__/operate.test.js
@@ -1,28 +1,23 @@
-import operate from "../operate";
+import operate from '../operate';
 
-describe("Testing operate function", () => {
-  it("testing add operation", () => {
-   
-    let result = operate("45", "15", "+");
-    expect(result).toBe("60");
+describe('Testing operate function', () => {
+  it('testing add operation', () => {
+    const result = operate('45', '15', '+');
+    expect(result).toBe('60');
   });
 
-  it("testing divide operation", () => {
-   
-    let result = operate("45", "15", "÷");
-    expect(result).toBe("3");
+  it('testing divide operation', () => {
+    const result = operate('45', '15', '÷');
+    expect(result).toBe('3');
   });
 
-  it("testing multiply operation", () => {
-    let result = operate("45", "2", "x");
-    expect(result).toBe("90");
+  it('testing multiply operation', () => {
+    const result = operate('45', '2', 'x');
+    expect(result).toBe('90');
   });
 
-  it("testing substract operation", () => {
-   
-    let result = operate("45", "15", "-");
-    expect(result).toBe("30");
+  it('testing substract operation', () => {
+    const result = operate('45', '15', '-');
+    expect(result).toBe('30');
   });
-
 });
-
