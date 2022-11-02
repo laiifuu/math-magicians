99da85dcfc88cab19061e577f1095397b877d2a8
Hayet Fellah <hayet.fellah145@gmail.com>
2022-11-02 16:26:21 +0100

    Add id to the button component

--- src/components/__test__/Button.test.js
+++ src/components/__test__/Button.test.js
@@ -6,7 +6,7 @@ const returnState = jest.fn();
 describe('Testing Button component', () => {
   it('renders Button component correctly', () => {
     const tree = renderer
-      .create(<Button className="width_25 grey center-text" value="AC" onClick={returnState} />)
+      .create(<Button className="width_25 grey center-text" id="AC" value="AC" onClick={returnState} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
